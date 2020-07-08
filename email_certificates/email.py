73b44f0d26d7565b89170e30057d1de15a9b086f
Solomon Himelbloom <2001solomon@gmail.com>
2020-07-08 10:30:34 -0800

    Initial commit for email certificates project

---
+++ email_certificates/email.py
@@ -0,0 +1,6 @@
+# AWS Educate – Project Templates
+#
+# An automated certificate generator for AWS Educate.
+# Website: https://aws.amazon.com/education/awseducate/student-ambassador-program/
+
+print("AWS Educate – Project Templates")
